67cdf85b076c6c0169a1f432f883c8ffec5c27b2
codingwithimran <codingwithimran@gmail.com>
2023-06-04 21:27:07 +0500

    problem fixed 06.04.2023:

--- app/src/main/java/com/codingwithimran/adminpanelecommerce/Adapters/LuckyDrawAdapter.java
+++ app/src/main/java/com/codingwithimran/adminpanelecommerce/Adapters/LuckyDrawAdapter.java
@@ -5,22 +5,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.codingwithimran.adminpanelecommerce.Modals.LuckDrawModal;
 import com.codingwithimran.adminpanelecommerce.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 public class LuckyDrawAdapter extends RecyclerView.Adapter<LuckyDrawAdapter.viewHolder>{
     Context context;
     ArrayList<LuckDrawModal> list;
-
+    FirebaseFirestore db;
+    FirebaseAuth auth;
     public LuckyDrawAdapter(Context context, ArrayList<LuckDrawModal> list) {
         this.context = context;
         this.list = list;
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
     }
 
     @NonNull
@@ -34,6 +42,25 @@ public class LuckyDrawAdapter extends RecyclerView.Adapter<LuckyDrawAdapter.view
         LuckDrawModal modal = list.get(position);
         holder.name.setText(modal.getMy_name());
         holder.email.setText(modal.getMy_email());
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                String id = modal.getId();
+                db.collection("Luck Draw").document(id).delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()) {
+                                    list.remove(modal);
+                                    notifyDataSetChanged();
+                                    Toast.makeText(context, "Lucky draw has been deleted", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                return false;
+            }
+        });
     }
 
     @Override
